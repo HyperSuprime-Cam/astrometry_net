9df6df089216aa40e15940b3fe4ea2662eb6089a
Mario Juric <mjuric@lsst.org>
2015-05-16 15:10:39 -0700

    Use $RLIBDIR to look for libraries

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -21,14 +21,14 @@ prep()
 
 build()
 {
-	make -j$NJOBS SYSTEM_GSL=yes GSL_INC="-I${GSL_DIR}/include" GSL_LIB="-L${GSL_DIR}/lib -lgsl -lgslcblas" WCSLIB_INC="-I${WCSLIB_DIR}/include/wcslib" WCSLIB_LIB="-L${WCSLIB_DIR}/lib -lwcs" CFITS_INC="-I${CFITSIO_DIR}/include" CFITS_LIB="-L${CFITSIO_DIR}/lib -lcfitsio"
+	make -j$NJOBS SYSTEM_GSL=yes GSL_INC="-I${GSL_DIR}/include" GSL_LIB="-L${GSL_DIR}/${RLIBDIR} -lgsl -lgslcblas" WCSLIB_INC="-I${WCSLIB_DIR}/include/wcslib" WCSLIB_LIB="-L${WCSLIB_DIR}/${RLIBDIR} -lwcs" CFITS_INC="-I${CFITSIO_DIR}/include" CFITS_LIB="-L${CFITSIO_DIR}/${RLIBDIR} -lcfitsio"
 }
 
 install()
 {
 	clean_old_install
 
-	( INSTALL_DIR="$PREFIX" make -j$NJOBS install SYSTEM_GSL=yes GSL_INC="-I${GSL_DIR}/include" GSL_LIB="-L${GSL_DIR}/lib -lgsl -lgslcblas" WCSLIB_INC="-I${WCSLIB_DIR}/include/wcslib" WCSLIB_LIB="-L${WCSLIB_DIR}/lib -lwcs" CFITS_INC="-I${CFITSIO_DIR}/include" CFITS_LIB="-L${CFITSIO_DIR}/lib -lcfitsio" )
+	( INSTALL_DIR="$PREFIX" make -j$NJOBS install SYSTEM_GSL=yes GSL_INC="-I${GSL_DIR}/include" GSL_LIB="-L${GSL_DIR}/${RLIBDIR} -lgsl -lgslcblas" WCSLIB_INC="-I${WCSLIB_DIR}/include/wcslib" WCSLIB_LIB="-L${WCSLIB_DIR}/${RLIBDIR} -lwcs" CFITS_INC="-I${CFITSIO_DIR}/include" CFITS_LIB="-L${CFITSIO_DIR}/${RLIBDIR} -lcfitsio" )
 
 	install_ups
 }
